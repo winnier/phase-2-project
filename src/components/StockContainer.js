e166df3d3ec8041e62384d566135ca0a918c2bb1
Winnie Mendoza <wrm956@gmail.com>
2022-06-21 17:51:11 -0400

    resolved changes to add feature

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,15 +1,19 @@
 import React from "react";
 import StockCard from "./StockCard";
 
-function StockContainer({ stockDb }) {
+
+function StockContainer({ stockDb, handleAddToPortfolio }) {
+
   
   const stockList = stockDb.map(item => {
-    return <StockCard key={item.Symbol} stock={item} />
+    return <StockCard key={item.Symbol} stock={item} handleAddToPortfolio={handleAddToPortfolio}/>
   })
   return (
     <div>
       <h2>Stocks</h2>
-      {stockList}
+      <div>
+        {stockList}
+      </div>
     </div>
   );
 }
