0f8ddc1eafe2ad9edf618285a3250df3bf5c8dff
Eric Ekas <eric.ekas@gmail.com>
2022-06-21 15:02:47 -0400

    updated stockcard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import './App.css';
-import { useEffect } from 'react';
 import NavBar from './components/NavBar'
-import StockPage from "./components/StockPage"
+import StockPage from './components/StockPage'
 import MyPortfolio from './components/MyPortfolio'
 import SearchBar from './components/SearchBar'
 
@@ -14,9 +13,12 @@ function App() {
 
   let pull_function = 'GLOBAL_QUOTE'
 
-  fetch(`https://www.alphavantage.co/query?function=${pull_function}&symbol=${ticker}&apikey=A85G8HED7A54MTDR`)
-  .then(res => res.json())
-  .then((data) => console.log(data))
+  const [stockDb, setStockDb] = useState([])
+  const [watchlist, setWatchlist] = useState([])
+
+  // fetch(`https://www.alphavantage.co/query?function=${pull_function}&symbol=${ticker}&apikey=A85G8HED7A54MTDR`)
+  // .then(res => res.json())
+  // .then((data) => console.log(data))
 
   return (
     <div>
--- src/components/StockCard.js
+++ src/components/StockCard.js
@@ -7,7 +7,9 @@ function StockCard() {
       <div className="card">
         <div className="card-body">
           <h5 className="card-title">{"Company Name"}</h5>
+          <p className="card-ticker">{"Stock Ticker"}</p>
           <p className="card-text">{"Stock Price"}</p>
+          <p className="card-sector">{"Stock Sector"}</p>
         </div>
       </div>
     </div>
