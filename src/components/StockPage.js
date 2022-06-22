07f6bde6c47579bb5d3ffbd1c79704d0c6e16cf1
Eric Ekas <eric.ekas@gmail.com>
2022-06-22 15:51:04 -0400

    more css

--- src/components/StockPage.js
+++ src/components/StockPage.js
@@ -9,13 +9,13 @@ function StockPage({ displayList, handleAddToPortfolio, handleSearch }) {
 
     return (
         <div className="stock-page">
-            <div className="search-container">
-                <SearchBar handleSearch={handleSearch}/>
-            </div>
             <div className="all-stocks-container">
                 <div className="col-8">
                 <StockContainer displayList={displayList} handleAddToPortfolio={handleAddToPortfolio} />
                 </div>
+            </div>
+            <div className="search-container">
+                <SearchBar handleSearch={handleSearch}/>
             </div>
         </div>
     )
